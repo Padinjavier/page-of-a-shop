385ae29118eda09d0ea1538e6e1e7f10c6c2d50d
javier <javierpadin661@gmail.com>
2022-06-15 20:19:54 -0500

    main final

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,54 +1,55 @@
 // //----------------------------para el tema eoscuro o claro --------------------------------
-// const lightTheme = {
-//   "--main-color": " #f3f6fd",
-//   "--white-darck": " #000000",
-//   '--sombra-header': '#00070767',
-//   '--varrita-scrol': '#d1802d',
-//   '--varra-scrol':'#9E9E9E',
-// };
-// const darkTheme = {
-//   "--main-color": " #111827",
-//   "--white-darck": " #f8f8f8",
-//   '--sombra-header': '#03f3f367',
-//   '--varrita-scrol': '#00fafa',
-//   '--varra-scrol':'#706e6e',
-// };
-// //--------------------boton de modo dia noche abajo ---------------------------
-// const boton = document.querySelector(".bubbly-button");
-// //detecta si el sistema tiene el modo dark o nigh
-// let darkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
-// const addTheme = (theme) => {
-//   const styles = document.documentElement.style;
-//   const customStyles = Object.keys(theme);
-//   for (const style of customStyles) {
-//     styles.setProperty(style, theme[style]);
-//   }
-// };
+const lightTheme = {
+  '--font-family':' "Quicksand", sans-serif',
+  '--card-width': '12.5rem',
+  '--card-height': '18.75rem',
+ ' --card-transition-duration':'800ms',
+ ' --card-transition-easing': 'ease',
+  '--white':'#ffffff',
+};
+const darkTheme = {
+  '--font-family': '"Quicksand", sans-serif',
+  '--card-width': '12.5rem',
+  '--card-height': '18.75rem',
+  '--card-transition-duration': '800ms',
+  '--card-transition-easing':'ease',
+  '--white':'#ffffff',
+};
+//--------------------boton de modo dia noche abajo ---------------------------
+const boton = document.querySelector(".bubbly-button");
+//detecta si el sistema tiene el modo dark o nigh
+let darkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+const addTheme = (theme) => {
+  const styles = document.documentElement.style;
+  const customStyles = Object.keys(theme);
+  for (const style of customStyles) {
+    styles.setProperty(style, theme[style]);
+  }
+};
 
-// const sun = document.querySelector("#sol");
-// const moon = document.querySelector("#luna");
+const sun = document.querySelector("#sol");
+const moon = document.querySelector("#luna");
 
-// if (darkMode == true) {
-//   sun.classList.add('active')
-//   moon.classList.remove('active')
-// } else {
-//   sun.classList.remove('active')
-//   moon.classList.add('active')
-// }
-
-// boton.addEventListener("click", () => {
-//   darkMode = !darkMode;
-//   // darkMode ? addTheme(darkTheme) : ;
-//   if (darkMode) {
-//     addTheme(darkTheme)
-//     sun.classList.add('active')
-//     moon.classList.remove('active')
-//   } else {
-//     addTheme(lightTheme)
-//     sun.classList.remove('active')
-//     moon.classList.add('active')
-//   }
-// });
+if (darkMode == true) {
+  sun.classList.add('active')
+  moon.classList.remove('active')
+} else {
+  sun.classList.remove('active')
+  moon.classList.add('active')
+}
+boton.addEventListener("click", () => {
+  darkMode = !darkMode;
+  // darkMode ? addTheme(darkTheme) : ;
+  if (darkMode) {
+    addTheme(darkTheme)
+    sun.classList.add('active')
+    moon.classList.remove('active')
+  } else {
+    addTheme(lightTheme)
+    sun.classList.remove('active')
+    moon.classList.add('active')
+  }
+});
 
 
 //-------------------------efecto del header que se esconde al scrolear---------------
